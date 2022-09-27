0ccb7fa332e468392e8d5bc62484fe55dcd94291
ggabrielaoliveira <gabi.oliveiraa@yahoo.com>
2022-09-27 11:52:24 -0300

    exercicio1 e 2

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const frutas = ["laranja", "limão", "uva"];
+
+const frutaUm = document.getElementById("fruta-1");
+frutaUm.innerHTML += frutas[0];
+
+const frutaDois = document.getElementById("fruta-2");
+frutaDois.innerHTML += frutas[1];
+
+const frutaTres = document.getElementById("fruta-3");
+frutaTres.innerHTML += frutas[2];
+
+const frutaQuatro = document.getElementById("fruta-4");
+frutaQuatro.innerHTML += frutas[3];
+
